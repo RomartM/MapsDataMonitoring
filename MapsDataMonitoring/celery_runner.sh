194f0c07a5e3f8988f026c5b350ace74524b4c7a
romart <mediantekid@gmail.com>
2019-01-07 08:02:55 +0800

    Unite Django Server and Celery Scheduler Startup

--- MapsDataMonitoring/celery_runner.sh
+++ MapsDataMonitoring/celery_runner.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Run Django Server (Defualt Port:8000)
+./manage.py runserver &
+# Run Celery Scheduler
 celery -A MapsDataMonitoring beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 celery -A MapsDataMonitoring worker -l info &
 tail -f /dev/null
