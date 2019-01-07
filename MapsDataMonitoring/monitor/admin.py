dad94883968c7418df6e1f55ec714e08cc04a732
romart <mediantekid@gmail.com>
2019-01-07 08:01:09 +0800

    Alter Django Admin Names

--- MapsDataMonitoring/monitor/admin.py
+++ MapsDataMonitoring/monitor/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import DataSet, SiteManifest, DataReport, EmailList
 
 
+# Alter Django default admin name
+admin.site.site_header = 'Maps Data Monitoring Administration'
+admin.site.site_title = 'Maps Data Monitoring Site Panel'
+
+
 # Add Data Set Model to Django Admin
 @admin.register(DataSet)
 class DataSetAdmin(admin.ModelAdmin):
